bbcff3d80d5171f195c15e969c39c515595a7c06
furszy <matiasfurszyfer@protonmail.com>
2020-06-28 21:04:15 -0300

    Fix insanity of CWalletDB::WriteTx and CWalletTx::WriteToDisk
    Coming from btc@0fd599767d2dabf25ac8c3543cb586cfc4b9d816

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -932,8 +932,6 @@ public:
     bool IsTrusted() const;
     bool IsTrusted(int& nDepth, bool& fConflicted) const;
 
-    bool WriteToDisk(CWalletDB *pwalletdb);
-
     int64_t GetTxTime() const;
     void UpdateTimeSmart();
     int GetRequestCount() const;
