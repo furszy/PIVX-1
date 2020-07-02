46dd507bf99442683fb8bdbc18bf56321daf3a48
furszy <matiasfurszyfer@protonmail.com>
2020-07-02 20:24:56 -0300

    Performance Regression Fix: Pre-Allocate txChanged vector
    coming from btc@ec81881b86b9680fcdcc42fd3ba31f04b8d09714

--- src/main.cpp
+++ src/main.cpp
@@ -2735,7 +2735,7 @@ bool static ConnectTip(CValidationState& state, CBlockIndex* pindexNew, CBlock* 
     UpdateTip(pindexNew);
 
     for(unsigned int i=0; i < pblock->vtx.size(); i++) {
-        txChanged.push_back(std::make_tuple(pblock->vtx[i], pindexNew, i));
+        txChanged.emplace_back(pblock->vtx[i], pindexNew, i);
     }
 
     int64_t nTime6 = GetTimeMicros();
@@ -3024,12 +3024,15 @@ bool ActivateBestChain(CValidationState& state, CBlock* pblock, bool fAlreadyChe
 
     CBlockIndex* pindexNewTip = nullptr;
     CBlockIndex* pindexMostWork = nullptr;
+    std::vector<std::tuple<CTransaction,CBlockIndex*,int>> txChanged;
+    if (pblock)
+        txChanged.reserve(pblock->vtx.size());
     do {
+        txChanged.clear();
         boost::this_thread::interruption_point();
 
         const CBlockIndex *pindexFork;
         std::list<CTransaction> txConflicted;
-        std::vector<std::tuple<CTransaction,CBlockIndex*,int>> txChanged;
         bool fInitialDownload;
         while (true) {
             TRY_LOCK(cs_main, lockMain);
