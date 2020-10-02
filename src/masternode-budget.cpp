316268d6abb0cb85683b5d0cb754b6e2b5e63c02
random-zebra <random.zebra@protonmail.com>
2020-10-02 23:46:27 +0200

    [BUG] Immediate expiration of finalized budgets
    According to IsTransactionValid, remove finalized budgets when the
    nBlockEnd is below the current block (not after two superblock cycles).

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -1953,15 +1953,10 @@ bool CFinalizedBudget::CheckName()
 
 bool CFinalizedBudget::IsExpired(int nCurrentHeight)
 {
-    // Remove obsolete finalized budgets after some time
-    const int nBlocksPerCycle = Params().GetConsensus().nBudgetCycleBlocks;
-    const int nBlockStart = nCurrentHeight - nCurrentHeight % nBlocksPerCycle + nBlocksPerCycle;
-
-    // Remove budgets where the last payment (from max. 100) ends before 2 budget-cycles before the current one
-    const int nMaxAge = nBlockStart - (2 * nBlocksPerCycle);
-
-    if (GetBlockEnd() < nMaxAge) {
-        strInvalid = strprintf("(ends at block %ld) too old and obsolete", GetBlockEnd());
+    // Remove budgets after their last payment block
+    const int nBlockEnd = GetBlockEnd();
+    if (nBlockEnd < nCurrentHeight) {
+        strInvalid = strprintf("(ends at block %ld) too old and obsolete", nBlockEnd);
         return true;
     }
 
