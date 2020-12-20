9faa18c1d96eef0f237f04e5fe8d4a804575236c
furszy <matiasfurszyfer@protonmail.com>
2020-12-20 13:11:19 -0300

    scripted-diff: Replace boost::bind with std::bind
    -BEGIN VERIFY SCRIPT-
    for j in $(seq 1 6)
     do
      sed -i "s/ boost::placeholders::_${j}/ std::placeholders::_${j}/g" $(git grep --name-only " boost::placeholders::_${j}" -- '*.cpp' '*.h')
     done
     sed -i "s/boost::bind/std::bind/g" $(git grep --name-only boost::bind -- '*.cpp' '*.h')
     sed -i "s/boost::ref/std::ref/g" $(git grep --name-only boost::ref -- '*.cpp' '*.h')
     sed -i '/boost\/bind/d' $(git grep --name-only boost/bind)
     -END VERIFY SCRIPT-

--- src/init.cpp
+++ src/init.cpp
@@ -1230,8 +1230,8 @@ bool AppInitMain()
     }
 
     // Start the lightweight task scheduler thread
-    CScheduler::Function serviceLoop = boost::bind(&CScheduler::serviceQueue, &scheduler);
-    threadGroup.create_thread(boost::bind(&TraceThread<CScheduler::Function>, "scheduler", serviceLoop));
+    CScheduler::Function serviceLoop = std::bind(&CScheduler::serviceQueue, &scheduler);
+    threadGroup.create_thread(std::bind(&TraceThread<CScheduler::Function>, "scheduler", serviceLoop));
 
     // Initialize Sapling circuit parameters
     LoadSaplingParams();
@@ -1739,7 +1739,7 @@ bool AppInitMain()
     for (const std::string& strFile : gArgs.GetArgs("-loadblock")) {
         vImportFiles.push_back(strFile);;
     }
-    threadGroup.create_thread(boost::bind(&ThreadImport, vImportFiles));
+    threadGroup.create_thread(std::bind(&ThreadImport, vImportFiles));
 
     // Wait for genesis block to be processed
     LogPrintf("Waiting for genesis block to be imported...\n");
@@ -1848,7 +1848,7 @@ bool AppInitMain()
     LogPrintf("fLiteMode %d\n", fLiteMode);
     LogPrintf("Budget Mode %s\n", strBudgetMode.c_str());
 
-    threadGroup.create_thread(boost::bind(&ThreadCheckMasternodes));
+    threadGroup.create_thread(std::bind(&ThreadCheckMasternodes));
 
     if (ShutdownRequested()) {
         LogPrintf("Shutdown requested. Exiting.\n");
@@ -1916,7 +1916,7 @@ bool AppInitMain()
 
         // StakeMiner thread disabled by default on regtest
         if (gArgs.GetBoolArg("-staking", !Params().IsRegTestNet() && DEFAULT_STAKING)) {
-            threadGroup.create_thread(boost::bind(&ThreadStakeMinter));
+            threadGroup.create_thread(std::bind(&ThreadStakeMinter));
         }
     }
 #endif
