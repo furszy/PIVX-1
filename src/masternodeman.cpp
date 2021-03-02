e86468e6b6fd3dd2e044d0f90141947a9c9d6cc3
random-zebra <random.zebra@protonmail.com>
2021-03-02 14:59:52 +0100

    [BUG] Fix locking order between CDeterministicMNManager/CMasternodeman
    Legacy manager should never call the deterministic one, while holding
    its own mutex (e.g. LegacyMNObsolete)
    
    fixing
     POTENTIAL DEADLOCK DETECTED
     Previous lock order was:
      cs_main blockassembler.cpp:236 (in thread bitcoin-httpworker)
      (1) cs evo/deterministicmns.cpp:517 (in thread bitcoin-httpworker)
      (2) cs masternodeman.cpp:456 (in thread bitcoin-httpworker)
     Current lock order is:
      (2) cs masternodeman.cpp:244 (in thread pivx-masternodeman)
      (1) cs evo/deterministicmns.cpp:855 (in thread pivx-masternodeman)

--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -226,14 +226,14 @@ void CMasternodeMan::AskForMN(CNode* pnode, const CTxIn& vin)
 
 int CMasternodeMan::CheckAndRemove(bool forceExpiredRemoval)
 {
-    LOCK(cs);
-
     // Skip after legacy obsolete. !TODO: remove when transition to DMN is complete
     if (deterministicMNManager->LegacyMNObsolete()) {
         LogPrint(BCLog::MASTERNODE, "Removing all legacy mn due to SPORK 21\n");
         Clear();
         return 0;
     }
+
+    LOCK(cs);
 
     //remove inactive and outdated (or replaced by DMN)
     auto it = mapMasternodes.begin();
@@ -657,12 +657,6 @@ std::vector<std::pair<int64_t, MasternodeRef>> CMasternodeMan::GetMasternodeRank
 
 int CMasternodeMan::ProcessMNBroadcast(CNode* pfrom, CMasternodeBroadcast& mnb)
 {
-    // Skip after legacy obsolete. !TODO: remove when transition to DMN is complete
-    if (deterministicMNManager->LegacyMNObsolete()) {
-        LogPrint(BCLog::MASTERNODE, "mnb - skip obsolete message\n");
-        return 0;
-    }
-
     const uint256& mnbHash = mnb.GetHash();
     if (mapSeenMasternodeBroadcast.count(mnbHash)) { //seen
         masternodeSync.AddedMasternodeList(mnbHash);
@@ -700,12 +694,6 @@ int CMasternodeMan::ProcessMNBroadcast(CNode* pfrom, CMasternodeBroadcast& mnb)
 
 int CMasternodeMan::ProcessMNPing(CNode* pfrom, CMasternodePing& mnp)
 {
-    // Skip after legacy obsolete. !TODO: remove when transition to DMN is complete
-    if (deterministicMNManager->LegacyMNObsolete()) {
-        LogPrint(BCLog::MASTERNODE, "mnb - skip obsolete message\n");
-        return 0;
-    }
-
     const uint256& mnpHash = mnp.GetHash();
     if (mapSeenMasternodePing.count(mnpHash)) return 0; //seen
 
@@ -732,12 +720,6 @@ int CMasternodeMan::ProcessMNPing(CNode* pfrom, CMasternodePing& mnp)
 
 int CMasternodeMan::ProcessGetMNList(CNode* pfrom, CTxIn& vin)
 {
-    // Skip after legacy obsolete. !TODO: remove when transition to DMN is complete
-    if (deterministicMNManager->LegacyMNObsolete()) {
-        LogPrint(BCLog::MASTERNODE, "dseg - skip obsolete message\n");
-        return 0;
-    }
-
     if (vin.IsNull()) { //only should ask for this once
         //local network
         bool isLocal = (pfrom->addr.IsRFC1918() || pfrom->addr.IsLocal());
@@ -856,7 +838,7 @@ void CMasternodeMan::UpdateMasternodeList(CMasternodeBroadcast& mnb)
     mapSeenMasternodeBroadcast.emplace(mnb.GetHash(), mnb);
     masternodeSync.AddedMasternodeList(mnb.GetHash());
 
-    LogPrint(BCLog::MASTERNODE,"CMasternodeMan::UpdateMasternodeList() -- masternode=%s\n", mnb.vin.prevout.ToString());
+    LogPrint(BCLog::MASTERNODE,"%s -- masternode=%s\n", __func__, mnb.vin.prevout.ToString());
 
     CMasternode* pmn = Find(mnb.vin.prevout);
     if (pmn == NULL) {
