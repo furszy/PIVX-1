ab0378edcfb3229da7ed131aecb17955b83cce1f
furszy <matiasfurszyfer@protonmail.com>
2021-05-17 15:27:04 -0300

    wallet: Create initial wallet backup after initialization. Plus fix "Failed to create backup..." error at startup when the wallet hasn't been created yet.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4102,7 +4102,7 @@ void CWallet::LockIfMyCollateral(const CTransactionRef& ptx)
     }
 }
 
-CWallet* CWallet::CreateWalletFromFile(const std::string walletFile)
+CWallet* CWallet::CreateWalletFromFile(const std::string& walletFile, bool& fFirstRun)
 {
     // needed to restore wallet transaction meta data after -zapwallettxes
     std::vector<CWalletTx> vWtx;
@@ -4126,7 +4126,7 @@ CWallet* CWallet::CreateWalletFromFile(const std::string walletFile)
     fVerifyingBlocks = true;
 
     int64_t nStart = GetTimeMillis();
-    bool fFirstRun = true;
+    fFirstRun = true;
     std::unique_ptr<CWalletDBWrapper> dbw(new CWalletDBWrapper(&bitdb, walletFile));
     CWallet *walletInstance = new CWallet(std::move(dbw));
     DBErrors nLoadWalletRet = walletInstance->LoadWallet(fFirstRun);
