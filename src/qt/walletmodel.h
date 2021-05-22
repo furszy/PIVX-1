cccac774ddaaf46265e44384331e3c04eb82ca79
furszy <matiasfurszyfer@protonmail.com>
2021-05-22 15:09:25 -0300

    Governance model: createAndSendProposalFeeTx method created.

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -27,6 +27,7 @@
 #include <QSettings>
 
 class AddressTableModel;
+class CBudgetProposal;
 class ClientModel;
 class OptionsModel;
 class RecentRequestsTableModel;
@@ -218,6 +219,8 @@ public:
     OperationResult PrepareShieldedTransaction(WalletModelTransaction* modelTransaction,
                                                             bool fromTransparent,
                                                             const CCoinControl* coinControl = nullptr);
+
+    OperationResult createAndSendProposalFeeTx(CBudgetProposal& prop);
 
     // Wallet encryption
     bool setWalletEncrypted(bool encrypted, const SecureString& passphrase);
