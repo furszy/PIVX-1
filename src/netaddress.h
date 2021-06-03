8c93cbedf71f99f8b174f404f60d6b9eb75520ba
furszy <matiasfurszyfer@protonmail.com>
2021-06-03 10:50:12 -0300

    [Refactor] netaddress: Stop suffering doing backports and align the header file to upstream's spacing format.
    This will make cherry-picks much much simpler to do. Otherwise every tiny cherry-pick conflicts with the entire class..

--- src/netaddress.h
+++ src/netaddress.h
@@ -31,139 +31,139 @@ enum Network {
 /** IP address (IPv6, or IPv4 using mapped IPv6 range (::FFFF:0:0/96)) */
 class CNetAddr
 {
-protected:
-    unsigned char ip[16]; // in network byte order
-    uint32_t scopeId{0};  // for scoped/link-local ipv6 addresses
+    protected:
+        unsigned char ip[16]; // in network byte order
+        uint32_t scopeId{0}; // for scoped/link-local ipv6 addresses
 
-public:
-    CNetAddr();
-    CNetAddr(const struct in_addr& ipv4Addr);
-    void Init();
-    void SetIP(const CNetAddr& ip);
+    public:
+        CNetAddr();
+        CNetAddr(const struct in_addr& ipv4Addr);
+        void Init();
+        void SetIP(const CNetAddr& ip);
 
-    /**
+        /**
          * Set raw IPv4 or IPv6 address (in network byte order)
          * @note Only NET_IPV4 and NET_IPV6 are allowed for network.
          */
-    void SetRaw(Network network, const uint8_t* data);
+        void SetRaw(Network network, const uint8_t* data);
 
-    /**
-      * Transform an arbitrary string into a non-routable ipv6 address.
-      * Useful for mapping resolved addresses back to their source.
-    */
-    bool SetInternal(const std::string& name);
+        /**
+          * Transform an arbitrary string into a non-routable ipv6 address.
+          * Useful for mapping resolved addresses back to their source.
+        */
+        bool SetInternal(const std::string& name);
 
-    bool SetSpecial(const std::string& strName); // for Tor addresses
-    bool IsIPv4() const;                         // IPv4 mapped address (::FFFF:0:0/96, 0.0.0.0/0)
-    bool IsIPv6() const;                         // IPv6 address (not mapped IPv4, not Tor)
-    bool IsRFC1918() const;                      // IPv4 private networks (10.0.0.0/8, 192.168.0.0/16, 172.16.0.0/12)
-    bool IsRFC2544() const;                      // IPv4 inter-network communcations (192.18.0.0/15)
-    bool IsRFC6598() const;                      // IPv4 ISP-level NAT (100.64.0.0/10)
-    bool IsRFC5737() const;                      // IPv4 documentation addresses (192.0.2.0/24, 198.51.100.0/24, 203.0.113.0/24)
-    bool IsRFC3849() const;                      // IPv6 documentation address (2001:0DB8::/32)
-    bool IsRFC3927() const;                      // IPv4 autoconfig (169.254.0.0/16)
-    bool IsRFC3964() const;                      // IPv6 6to4 tunnelling (2002::/16)
-    bool IsRFC4193() const;                      // IPv6 unique local (FC00::/7)
-    bool IsRFC4380() const;                      // IPv6 Teredo tunnelling (2001::/32)
-    bool IsRFC4843() const;                      // IPv6 ORCHID (2001:10::/28)
-    bool IsRFC4862() const;                      // IPv6 autoconfig (FE80::/64)
-    bool IsRFC6052() const;                      // IPv6 well-known prefix (64:FF9B::/96)
-    bool IsRFC6145() const;                      // IPv6 IPv4-translated address (::FFFF:0:0:0/96)
-    bool IsTor() const;
-    bool IsLocal() const;
-    bool IsRoutable() const;
-    bool IsInternal() const;
-    bool IsValid() const;
-    enum Network GetNetwork() const;
-    std::string ToString() const;
-    std::string ToStringIP() const;
-    unsigned int GetByte(int n) const;
-    uint64_t GetHash() const;
-    bool GetInAddr(struct in_addr* pipv4Addr) const;
-    uint32_t GetNetClass() const;
+        bool SetSpecial(const std::string& strName); // for Tor addresses
+        bool IsIPv4() const;    // IPv4 mapped address (::FFFF:0:0/96, 0.0.0.0/0)
+        bool IsIPv6() const;    // IPv6 address (not mapped IPv4, not Tor)
+        bool IsRFC1918() const; // IPv4 private networks (10.0.0.0/8, 192.168.0.0/16, 172.16.0.0/12)
+        bool IsRFC2544() const; // IPv4 inter-network communications (198.18.0.0/15)
+        bool IsRFC6598() const; // IPv4 ISP-level NAT (100.64.0.0/10)
+        bool IsRFC5737() const; // IPv4 documentation addresses (192.0.2.0/24, 198.51.100.0/24, 203.0.113.0/24)
+        bool IsRFC3849() const; // IPv6 documentation address (2001:0DB8::/32)
+        bool IsRFC3927() const; // IPv4 autoconfig (169.254.0.0/16)
+        bool IsRFC3964() const; // IPv6 6to4 tunnelling (2002::/16)
+        bool IsRFC4193() const; // IPv6 unique local (FC00::/7)
+        bool IsRFC4380() const; // IPv6 Teredo tunnelling (2001::/32)
+        bool IsRFC4843() const; // IPv6 ORCHID (2001:10::/28)
+        bool IsRFC4862() const; // IPv6 autoconfig (FE80::/64)
+        bool IsRFC6052() const; // IPv6 well-known prefix (64:FF9B::/96)
+        bool IsRFC6145() const; // IPv6 IPv4-translated address (::FFFF:0:0:0/96)
+        bool IsTor() const;
+        bool IsLocal() const;
+        bool IsRoutable() const;
+        bool IsInternal() const;
+        bool IsValid() const;
+        enum Network GetNetwork() const;
+        std::string ToString() const;
+        std::string ToStringIP() const;
+        unsigned int GetByte(int n) const;
+        uint64_t GetHash() const;
+        bool GetInAddr(struct in_addr* pipv4Addr) const;
+        uint32_t GetNetClass() const;
 
-    //! For IPv4, mapped IPv4, SIIT translated IPv4, Teredo, 6to4 tunneled addresses, return the relevant IPv4 address as a uint32.
-    uint32_t GetLinkedIPv4() const;
-    //! Whether this address has a linked IPv4 address (see GetLinkedIPv4()).
-    bool HasLinkedIPv4() const;
+        //! For IPv4, mapped IPv4, SIIT translated IPv4, Teredo, 6to4 tunneled addresses, return the relevant IPv4 address as a uint32.
+        uint32_t GetLinkedIPv4() const;
+        //! Whether this address has a linked IPv4 address (see GetLinkedIPv4()).
+        bool HasLinkedIPv4() const;
 
-    // The AS on the BGP path to the node we use to diversify
-    // peers in AddrMan bucketing based on the AS infrastructure.
-    // The ip->AS mapping depends on how asmap is constructed.
-    uint32_t GetMappedAS(const std::vector<bool> &asmap) const;
+        // The AS on the BGP path to the node we use to diversify
+        // peers in AddrMan bucketing based on the AS infrastructure.
+        // The ip->AS mapping depends on how asmap is constructed.
+        uint32_t GetMappedAS(const std::vector<bool> &asmap) const;
 
-    std::vector<unsigned char> GetGroup(const std::vector<bool> &asmap) const;
-    int GetReachabilityFrom(const CNetAddr* paddrPartner = nullptr) const;
+        std::vector<unsigned char> GetGroup(const std::vector<bool> &asmap) const;
+        int GetReachabilityFrom(const CNetAddr* paddrPartner = nullptr) const;
 
-    CNetAddr(const struct in6_addr& pipv6Addr, const uint32_t scope = 0);
-    bool GetIn6Addr(struct in6_addr* pipv6Addr) const;
+        CNetAddr(const struct in6_addr& pipv6Addr, const uint32_t scope = 0);
+        bool GetIn6Addr(struct in6_addr* pipv6Addr) const;
 
-    friend bool operator==(const CNetAddr& a, const CNetAddr& b);
-    friend bool operator!=(const CNetAddr& a, const CNetAddr& b);
-    friend bool operator<(const CNetAddr& a, const CNetAddr& b);
+        friend bool operator==(const CNetAddr& a, const CNetAddr& b);
+        friend bool operator!=(const CNetAddr& a, const CNetAddr& b);
+        friend bool operator<(const CNetAddr& a, const CNetAddr& b);
 
-    SERIALIZE_METHODS(CNetAddr, obj) { READWRITE(obj.ip); }
+        SERIALIZE_METHODS(CNetAddr, obj) { READWRITE(obj.ip); }
 
-    friend class CSubNet;
+        friend class CSubNet;
 };
 
 class CSubNet
 {
-protected:
-    /// Network (base) address
-    CNetAddr network;
-    /// Netmask, in network byte order
-    uint8_t netmask[16];
-    /// Is this value valid? (only used to signal parse errors)
-    bool valid;
+    protected:
+        /// Network (base) address
+        CNetAddr network;
+        /// Netmask, in network byte order
+        uint8_t netmask[16];
+        /// Is this value valid? (only used to signal parse errors)
+        bool valid;
 
-public:
-    CSubNet();
-    CSubNet(const CNetAddr& addr, int32_t mask);
-    CSubNet(const CNetAddr& addr, const CNetAddr& mask);
+    public:
+        CSubNet();
+        CSubNet(const CNetAddr& addr, int32_t mask);
+        CSubNet(const CNetAddr& addr, const CNetAddr& mask);
 
-    //constructor for single ip subnet (<ipv4>/32 or <ipv6>/128)
-    explicit CSubNet(const CNetAddr& addr);
+        //constructor for single ip subnet (<ipv4>/32 or <ipv6>/128)
+        explicit CSubNet(const CNetAddr& addr);
 
-    bool Match(const CNetAddr& addr) const;
+        bool Match(const CNetAddr& addr) const;
 
-    std::string ToString() const;
-    bool IsValid() const;
+        std::string ToString() const;
+        bool IsValid() const;
 
-    friend bool operator==(const CSubNet& a, const CSubNet& b);
-    friend bool operator!=(const CSubNet& a, const CSubNet& b);
-    friend bool operator<(const CSubNet& a, const CSubNet& b);
+        friend bool operator==(const CSubNet& a, const CSubNet& b);
+        friend bool operator!=(const CSubNet& a, const CSubNet& b);
+        friend bool operator<(const CSubNet& a, const CSubNet& b);
 
-    SERIALIZE_METHODS(CSubNet, obj) { READWRITE(obj.network, obj.netmask, obj.valid); }
+        SERIALIZE_METHODS(CSubNet, obj) { READWRITE(obj.network, obj.netmask, obj.valid); }
 };
 
 /** A combination of a network address (CNetAddr) and a (TCP) port */
 class CService : public CNetAddr
 {
-protected:
-    uint16_t port; // host order
+    protected:
+        uint16_t port; // host order
 
-public:
-    CService();
-    CService(const CNetAddr& ip, uint16_t port);
-    CService(const struct in_addr& ipv4Addr, uint16_t port);
-    CService(const struct sockaddr_in& addr);
-    void Init();
-    uint16_t GetPort() const;
-    bool GetSockAddr(struct sockaddr* paddr, socklen_t* addrlen) const;
-    bool SetSockAddr(const struct sockaddr* paddr);
-    friend bool operator==(const CService& a, const CService& b);
-    friend bool operator!=(const CService& a, const CService& b);
-    friend bool operator<(const CService& a, const CService& b);
-    std::vector<unsigned char> GetKey() const;
-    std::string ToString() const;
-    std::string ToStringPort() const;
-    std::string ToStringIPPort() const;
+    public:
+        CService();
+        CService(const CNetAddr& ip, uint16_t port);
+        CService(const struct in_addr& ipv4Addr, uint16_t port);
+        CService(const struct sockaddr_in& addr);
+        void Init();
+        uint16_t GetPort() const;
+        bool GetSockAddr(struct sockaddr* paddr, socklen_t* addrlen) const;
+        bool SetSockAddr(const struct sockaddr* paddr);
+        friend bool operator==(const CService& a, const CService& b);
+        friend bool operator!=(const CService& a, const CService& b);
+        friend bool operator<(const CService& a, const CService& b);
+        std::vector<unsigned char> GetKey() const;
+        std::string ToString() const;
+        std::string ToStringPort() const;
+        std::string ToStringIPPort() const;
 
-    CService(const struct in6_addr& ipv6Addr, uint16_t port);
-    CService(const struct sockaddr_in6& addr);
+        CService(const struct in6_addr& ipv6Addr, uint16_t port);
+        CService(const struct sockaddr_in6& addr);
 
-    SERIALIZE_METHODS(CService, obj) { READWRITE(obj.ip, Using<BigEndianFormatter<2>>(obj.port)); }
+        SERIALIZE_METHODS(CService, obj) { READWRITE(obj.ip, Using<BigEndianFormatter<2>>(obj.port)); }
 };
 
 #endif // PIVX_NETADDRESS_H
